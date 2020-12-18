8321877000ab92278639cdec79ca97365dba0943
Luana Barros <dev.luana@gmail.com>
2020-12-18 11:46:38 -0300

    ewe

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-print('entrou')
 import socket
+import os
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
