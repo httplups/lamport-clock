e6c3c3fa2373f7a8c5d4e5bb8e6839c17701796b
Luana Barros <dev.luana@gmail.com>
2020-12-18 12:03:24 -0300

    dsdds

--- google.py
+++ google.py
@@ -12,6 +12,7 @@ port = 80
 
 try:
     host_ip = socket.gethostbyname('www.google.com')
+    print(host_ip, type(host_ip))
 except socket.gaierror:
     # this means could not resolve the host
     print "there was an error resolving the host"
