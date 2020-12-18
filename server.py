3dfe5cf5956d6f416483f90eafb515dc62fdba33
Luana Barros <dev.luana@gmail.com>
2020-12-18 11:45:34 -0300

    assasa

--- server.py
+++ server.py
@@ -5,6 +5,9 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+line = os.popen("curl ifconfig.me").readlines()
+print(line)
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
