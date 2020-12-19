0e10b0aa04707c9a9f467206bed217631b8a24be
Luana Barros <dev.luana@gmail.com>
2020-12-19 16:59:45 -0300

    counter como variavel global

--- server.py
+++ server.py
@@ -5,53 +5,54 @@ import _thread as thread
 import time
 import json
 stop_loop = False
+global counter
+counter = 0 # global var
 
-def local_time(counter):
+def local_time():
     return ' (LAMPORT_TIME={})'.format(counter)
 
-def calc_recv_timestamp(recv_time_stamp, counter):
+def calc_recv_timestamp(recv_time_stamp):
     return max(recv_time_stamp, counter) + 1
 
-def send_message(client_ip, counter, sock):
+def send_message(client_ip, sock):
+    global counter
     counter += 1
     data = json.dumps({"message":"Oi", "timestamp":counter})
     sock.send(data.encode())
-    print('Message sent to {} at {}'.format(client_ip,local_time(counter)))
-    return counter
+    print('Message sent to {} at {}'.format(client_ip,local_time()))
 
-def recv_message(client_ip, counter, sock, stop_loop):
+def recv_message(client_ip, sock, stop_loop):
     try:
         data = sock.recv(1024)
         data = json.loads(data.decode())
-        message = data.get("message")
+        # message = data.get("message")
         timestamp = data.get("timestamp")
-        counter = calc_recv_timestamp(timestamp, counter)
-        print('Message received from {} at {}'.format(client_ip,local_time(counter)))
+
+        global counter
+        counter = calc_recv_timestamp(timestamp)
+        print('Message received from {} at {}'.format(client_ip,local_time()))
     except ValueError:
         # acabou os bytes
         stop_loop = True
-    return [counter, stop_loop]
+    return stop_loop
 
+# usa counter como uma variavel global
 def handle_client(conn, client_ip):
-    counter = 0
     stop_loop = False
     with conn:
         print('Connected by', client_ip)
         while not stop_loop:
-            counter, stop_loop = recv_message(client_ip, counter, conn, stop_loop)
+            stop_loop = recv_message(client_ip, conn, stop_loop)
             if stop_loop:
                 break
             time.sleep(10)
-            counter = send_message(client_ip, counter, conn)
+            send_message(client_ip, conn)
         conn.close()
     
 if __name__ == '__main__': 
 
     HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
     PORT = 8888        # Port to listen on (non-privileged ports are > 1023)
-
-    file = open('ips.txt', 'r')
-    Lines = file.readlines()
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
