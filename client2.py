8ac098ae2d2b850b77053ffbc853648307ea04dc
Luana Barros <dev.luana@gmail.com>
2020-12-19 15:58:09 -0300

    dividi em 2 clientes diferentes

---
+++ client2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import socket
+import urllib.request
+
+
+def local_time(counter):
+    return ' (LAMPORT_TIME={})'.format(counter)
+
+def calc_recv_timestamp(recv_time_stamp, counter):
+    return max(recv_time_stamp, counter) + 1
+
+def event(name, counter):
+    counter += 1
+    # print('Something happened in {} !'.\
+    #       format(pid) + local_time(counter))
+    print('Something happened in {} '.format(name), local_time(counter))
+    return counter
+
+def send_message(name, counter, sock):
+    counter += 1
+    data = 'Hello from {name}'.format(name)
+    s.send((data,counter))
+    print('Message sent from {}'.format(name),local_time(counter))
+    return counter
+
+def recv_message(name, counter, sock):
+    message, timestamp = sock.recv(1024)
+    counter = calc_recv_timestamp(timestamp, counter)
+    print('Message received at {}'.format(name),local_time(counter))
+    return counter
+
+if __name__ == '__main__': 
+
+    HOST = '52.90.198.186'
+    PORT = 8888        # The port used by the server
+    counter = 0
+    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        counter = event(name=external_ip, counter)
+        counter = event(name=external_ip, counter)
+        counter = send_message(name=external_ip, counter, s)
+        counter = recv_message(name=external_ip, counter, s)
+        counter = event(name=external_ip, counter)
--- server.py
+++ server.py
@@ -1,30 +1,42 @@
 #!/usr/bin/env python3
 import socket
 import os
+import thread
+import time
 
-HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
-PORT = 8888        # Port to listen on (non-privileged ports are > 1023)
+def local_time(counter):
+    return ' (LAMPORT_TIME={})'.format(counter)
 
-file = open('ips.txt', 'r')
-Lines = file.readlines()
+def calc_recv_timestamp(recv_time_stamp, counter):
+    return max(recv_time_stamp, counter) + 1
 
-count = 0
-# Strips the newline character
-# for line in Lines:
-#     ip, name = line.split()
-#     print(ip)
-#     print(name)
-    #print("Line{}: {}".format(count, line.strip()))
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print('listening')
-    conn, addr = s.accept()
+def handle_client(conn, addr):
+    counter = 0
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.recv(1024)
-            if not data:
+            message, timestamp = conn.recv(1024)
+            if not message:
                 break
-            conn.sendall(data)
+            time.sleep(10)
+            counter += 1
+            conn.send((message, counter))
+        conn.close()
+    
+if __name__ == '__main__': 
+
+    HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
+    PORT = 8888        # Port to listen on (non-privileged ports are > 1023)
+
+    file = open('ips.txt', 'r')
+    Lines = file.readlines()
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print 'Server started!'
+        print 'Waiting for clients...'
+        while True:
+            conn, addr = s.accept()
+            thread.start_new_thread(handle_client,(conn,addr))
+        s.close()
