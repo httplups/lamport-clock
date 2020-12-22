f91744441e7639f33c18d505313c5ffa37e25ea9
Luana Barros <dev.luana@gmail.com>
2020-12-22 20:36:56 -0300

    asas

--- client1.py
+++ client1.py
@@ -18,7 +18,7 @@ def event(counter):
 
 def connect(counter, sock, HOST, PORT):
     print(HOST, PORT)
-    sock.connect(HOST, PORT)
+    sock.connect((HOST, PORT))
     counter += 1
     print('Connection Request at {}'.format(local_time(counter)))
     return counter
@@ -51,8 +51,9 @@ if __name__ == '__main__':
     print('This IP address is: ', external_ip)
     
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
         counter = event(counter)
-        counter = connect(counter, s, HOST, PORT)
+        # counter = connect(counter, s, HOST, PORT)
         counter = send_message(HOST, counter, s)
         counter = recv_message(HOST, counter, s)
         counter = send_message(HOST, counter, s)
